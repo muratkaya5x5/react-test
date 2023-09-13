6a36a8519a101d764eea139734f1b1475598abc6
Murat KAYA <kaya.murat@mke.gov.tr>
2023-09-13 09:26:29 +0300

    ders 2

--- src/App.js
+++ src/App.js
@@ -3,21 +3,15 @@ import './App.css';
 
 function App() {
   return (
+   
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h3>{process.env.NODE_ENV} </h3>
+    {process.env.NODE_ENV==='production' && (
+      <>     
+     <p> {process.env.REACT_APP_API_URL}  </p>   
+     </>
+    )}
+    <img src={logo} className="App-logo" alt="logo" />
     </div>
   );
 }
